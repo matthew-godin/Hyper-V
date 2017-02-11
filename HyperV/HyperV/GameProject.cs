3380fa5b6c86eb6d1db91ee8fdb456ad1974e46d
Matthew Godin <150533628+matthew-godin@users.noreply.github.com>
2017-02-10 23:52:22 -0500

    Grass class available

--- HyperV/HyperV/GameProject.cs
+++ HyperV/HyperV/GameProject.cs
@@ -10,6 +10,7 @@ using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
 using System.IO;
 using System.Diagnostics;
+using GameProjectXNA;
 
 namespace HyperV
 {
@@ -22,6 +23,14 @@ namespace HyperV
         SubjectiveCamera GameCamera { get; set; }                
         InputManager InputMgr { get; set; }
 
+        //GraphicsDeviceManager GraphicsMgr { get; set; }
+        SpriteBatch SpriteMgr { get; set; }
+
+        RessourcesManager<SpriteFont> FontMgr { get; set; }
+        RessourcesManager<Texture2D> TextureMgr { get; set; }
+        RessourcesManager<Model> ModelMgr { get; set; }
+        //Camera GameCamera { get; set; }
+
         public GameProject()
         {
             GraphicsMgr = new GraphicsDeviceManager(this);
@@ -33,23 +42,30 @@ namespace HyperV
 
         protected override void Initialize()
         {
+            const float OBJECT_SCALE = 0.01f;
+            Vector3 objectPosition = new Vector3(0, -10, -50);
+            Vector3 objectRotation = new Vector3(0, MathHelper.PiOver2, 0);
+            FontMgr = new RessourcesManager<SpriteFont>(this, "Fonts");
+            TextureMgr = new RessourcesManager<Texture2D>(this, "Textures");
+            ModelMgr = new RessourcesManager<Model>(this, "Models");
+            //GameCamera = new StableCamera(this, Vector3.Zero, objectPosition, Vector3.Up);
+            GameCamera = new SubjectiveCamera(this, Vector3.Zero, objectPosition, Vector3.Up, UPDATE_INTERVAL_STANDARD);
             InputMgr = new InputManager(this);
             Components.Add(InputMgr);
-            GameCamera = new SubjectiveCamera(this, Vector3.Zero, Vector3.Zero, Vector3.Up, UPDATE_INTERVAL_STANDARD);
+            Components.Add(new NightSkyBackground(this, "NightSky", UPDATE_INTERVAL_STANDARD));
             Components.Add(GameCamera);
-            Components.Add(new NightSkyBackground(this, "CielBlue", UPDATE_INTERVAL_STANDARD));
             Components.Add(new Displayer3D(this));
-            Components.Add(new FPSDisplay(this, "Arial", Color.Red, FPS_COMPUTE_INTERVAL));
-            Components.Add(new Niveau(this));
-
-            Services.AddService(typeof(Random), new Random());
-            Services.AddService(typeof(RessourcesManager<SpriteFont>), new RessourcesManager<SpriteFont>(this, "Fonts"));
-            Services.AddService(typeof(RessourcesManager<SoundEffect>), new RessourcesManager<SoundEffect>(this, "Sounds"));
-            Services.AddService(typeof(RessourcesManager<Song>), new RessourcesManager<Song>(this, "Songs"));
-            Services.AddService(typeof(RessourcesManager<Texture2D>), new RessourcesManager<Texture2D>(this, "Textures"));
+            Components.Add(new BaseObject(this, "ship", OBJECT_SCALE, objectRotation, objectPosition));
+            //Components.Add(new TexturePlane(this, 1f, Vector3.Zero, new Vector3(4, 4, -5), new Vector2(20, 20), new Vector2(40, 40), "Grass", UPDATE_INTERVAL_STANDARD));
+            Services.AddService(typeof(RessourcesManager<Texture2D>), TextureMgr);
+            Components.Add(new Grass(this, 1f, Vector3.Zero, new Vector3(0, 0, 0), new Vector2(256, 256), "Grass", UPDATE_INTERVAL_STANDARD));
+            Components.Add(new FPSDisplay(this, "Arial", Color.Tomato, FPS_COMPUTE_INTERVAL));
+            Services.AddService(typeof(RessourcesManager<SpriteFont>), FontMgr);
+            Services.AddService(typeof(RessourcesManager<Model>), ModelMgr);
             Services.AddService(typeof(InputManager), InputMgr);
-            Services.AddService(typeof(SubjectiveCamera), GameCamera);
-            Services.AddService(typeof(SpriteBatch), new SpriteBatch(GraphicsDevice));            
+            Services.AddService(typeof(Camera), GameCamera);
+            SpriteMgr = new SpriteBatch(GraphicsDevice);
+            Services.AddService(typeof(SpriteBatch), SpriteMgr);
             base.Initialize();
         }
         protected override void Update(GameTime gameTime)
---
+++ HyperV/HyperV/Grass.cs
@@ -0,0 +1,137 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using XNAProject;
+
+
+namespace HyperV
+{
+    /// <summary>
+    /// This is a game component that implements IUpdateable.
+    /// </summary>
+    public class Grass : BasicPrimitive
+    {
+        Vector3 Position { get; set; }
+        float UpdateInterval { get; set; }
+        protected InputManager InputMgr { get; private set; }
+        float TimeElapsedSinceUpdate { get; set; }
+
+        const int NUM_TRIANGLES = 2;
+        protected Vector3[,] VerticesPts { get; private set; }
+        Vector3 Origin { get; set; }
+        Vector2 Delta { get; set; }
+        protected BasicEffect BscEffect { get; private set; }
+
+        //VertexPositionColor[] Vertices { get; set; }
+        RessourcesManager<Texture2D> TextureMgr;
+        Texture2D TileTexture;
+        VertexPositionTexture[] Vertices { get; set; }
+        BlendState AlphaMgr { get; set; }
+
+        Vector2[,] TexturePts { get; set; }
+        string NomTileTexture { get; set; }
+
+        public Grass(Game game, float initialScale, Vector3 initialRotation, Vector3 initialPosition, Vector2 span, string nomTileTexture, float updateInterval) : base(game, initialScale, initialRotation, initialPosition)
+        {
+            NomTileTexture = nomTileTexture;
+            UpdateInterval = updateInterval;
+            Delta = new Vector2(span.X, span.Y);
+            Origin = new Vector3(-Delta.X / 2, 0, -Delta.Y / 2); //to center the primitive to point (0,0,0)
+        }
+
+        public override void Initialize()
+        {
+            NumVertices = NUM_TRIANGLES + 2;
+            VerticesPts = new Vector3[2, 2];
+            CreatePointArray();
+            CreateVertexArray();
+            Position = InitialPosition;
+            InputMgr = Game.Services.GetService(typeof(InputManager)) as InputManager;
+            TimeElapsedSinceUpdate = 0;
+            base.Initialize();
+        }
+
+        private void CreatePointArray()
+        {
+            VerticesPts[0, 0] = new Vector3(Origin.X, Origin.Y, Origin.Z);
+            VerticesPts[1, 0] = new Vector3(Origin.X - Delta.X, Origin.Y, Origin.Z);
+            VerticesPts[0, 1] = new Vector3(Origin.X, Origin.Y, Origin.Z + Delta.Y);
+            VerticesPts[1, 1] = new Vector3(Origin.X - Delta.X, Origin.Y, Origin.Z + Delta.Y);
+        }
+
+        protected void CreateVertexArray()
+        {
+            TexturePts = new Vector2[2, 2];
+            Vertices = new VertexPositionTexture[NumVertices];
+            CreatePointArrayTexture();
+        }
+
+        private void CreatePointArrayTexture()
+        {
+            TexturePts[0, 0] = new Vector2(0, 1);
+            TexturePts[1, 0] = new Vector2(1, 1);
+            TexturePts[0, 1] = new Vector2(0, 0);
+            TexturePts[1, 1] = new Vector2(1, 0);
+        }
+
+        protected override void LoadContent()
+        {
+            TextureMgr = Game.Services.GetService(typeof(RessourcesManager<Texture2D>)) as RessourcesManager<Texture2D>;
+            TileTexture = TextureMgr.Find(NomTileTexture);
+            BscEffect = new BasicEffect(GraphicsDevice);
+            InitializeBscEffectParameters();
+            base.LoadContent();
+        }
+
+        protected void InitializeBscEffectParameters()
+        {
+            //BscEffect.VertexColorEnabled = true;
+            BscEffect.TextureEnabled = true;
+            BscEffect.Texture = TileTexture;
+            AlphaMgr = BlendState.AlphaBlend; // Be careful of this...
+        }
+
+        protected override void InitializeVertices() // Is called by base.Initialize()
+        {
+            int VertexIndex = -1;
+            for (int j = 0; j < 1; ++j)
+            {
+                for (int i = 0; i < 2; ++i)
+                {
+                    //Vertices[++VertexIndex] = new VertexPositionColor(VerticesPts[i, j], Color.LawnGreen);
+                    //Vertices[++VertexIndex] = new VertexPositionColor(VerticesPts[i, j + 1], Color.LawnGreen);
+                    Vertices[++VertexIndex] = new VertexPositionTexture(VerticesPts[i, j], TexturePts[i, j]);
+                    Vertices[++VertexIndex] = new VertexPositionTexture(VerticesPts[i, j + 1], TexturePts[i, j + 1]);
+                }
+            }
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            BlendState oldBlendState = GraphicsDevice.BlendState; // ... and this!
+            GraphicsDevice.BlendState = AlphaMgr;
+            BscEffect.World = GetWorld();
+            BscEffect.View = GameCamera.View;
+            BscEffect.Projection = GameCamera.Projection;
+            foreach (EffectPass passEffect in BscEffect.CurrentTechnique.Passes)
+            {
+                passEffect.Apply();
+                DrawTriangleStrip();
+            }
+            GraphicsDevice.BlendState = oldBlendState;
+        }
+
+        protected void DrawTriangleStrip()
+        {
+            //GraphicsDevice.DrawUserPrimitives<VertexPositionColor>(PrimitiveType.TriangleStrip, Vertices, 0, NUM_TRIANGLES);
+            GraphicsDevice.DrawUserPrimitives<VertexPositionTexture>(PrimitiveType.TriangleStrip, Vertices, 0, NUM_TRIANGLES);
+        }
+    }
+}

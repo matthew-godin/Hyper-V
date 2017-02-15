02e32ae0eb861085c2c44eb5b088241b7e98a726
Raphael Brule <raphael_brule@hotmail.com>
2017-02-15 10:28:31 -0500

    PlayerCamera with mouse

--- HyperV/HyperV/PlayerCamera.cs
+++ HyperV/HyperV/PlayerCamera.cs
@@ -9,6 +9,7 @@ namespace HyperV
         const float STANDARD_UPDATE_INTERVAL = 1f / 60f;
         const float ACCELERATION = 0.001f;
         const float INITIAL_ROTATION_SPEED = 5f;
+        const float INITIAL_ROTATION_SPEED_SOURIS = 0.1f;
         const float TRANSLATION_INITIAL_SPEED = 0.5f;
         const float DELTA_YAW = MathHelper.Pi / 180; // 1 degré à la fois
         const float DELTA_ROLL = MathHelper.Pi / 180; // 1 degré à la fois
@@ -21,29 +22,13 @@ namespace HyperV
         Grass Grass { get; set; }
         float TranslationSpeed { get; set; }
         float SpeedRotation { get; set; }
+        Point PreviousMousePosition { get; set; }
+        Point CurrentMousePosition { get; set; }
+        Vector2 DisplacementMouse { get; set; }
 
         float UpdateInterval { get; set; }
         float TimeElapsedSinceUpdate { get; set; }
         InputManager InputMgr { get; set; }
-
-        bool inZoom;
-        bool InZoom
-        {
-            get { return inZoom; }
-            set
-            {
-                float aspectRatio = Game.GraphicsDevice.Viewport.AspectRatio;
-                inZoom = value;
-                if (inZoom)
-                {
-                    CreateViewingFrustum(OBJECTIVE_OPENNESS / 2, aspectRatio, NEAR_PLANE_DISTANCE, FAR_PLANE_DISTANCE);
-                }
-                else
-                {
-                    CreateViewingFrustum(OBJECTIVE_OPENNESS, aspectRatio, NEAR_PLANE_DISTANCE, FAR_PLANE_DISTANCE);
-                }
-            }
-        }
 
         public PlayerCamera(Game game, Vector3 cameraPosition, Vector3 target, Vector3 orientation, float updateInterval)
            : base(game)
@@ -51,7 +36,6 @@ namespace HyperV
             UpdateInterval = updateInterval;
             CreateViewingFrustum(OBJECTIVE_OPENNESS, NEAR_PLANE_DISTANCE, FAR_PLANE_DISTANCE);
             CreateLookAt(cameraPosition, target, orientation);
-            InZoom = false;
         }
 
         public override void Initialize()
@@ -62,6 +46,8 @@ namespace HyperV
             base.Initialize();
             InputMgr = Game.Services.GetService(typeof(InputManager)) as InputManager;
             Grass = Game.Services.GetService(typeof(Grass)) as Grass;
+            CurrentMousePosition = InputMgr.GetPositionMouse();
+            PreviousMousePosition = InputMgr.GetPositionMouse();
         }
 
         protected override void CreateLookAt()
@@ -91,43 +77,69 @@ namespace HyperV
         {
             float timeElapsed = (float)gameTime.ElapsedGameTime.TotalSeconds;
             TimeElapsedSinceUpdate += timeElapsed;
-            KeyboardManagement();
             if (TimeElapsedSinceUpdate >= UpdateInterval)
             {
-                if (InputMgr.IsPressed(Keys.LeftShift) || InputMgr.IsPressed(Keys.RightShift))
-                {
-                    ManageAcceleration();
-                    ManageDisplacement();
-                    ManageRotation();
-                    CreateLookAt();
-                    ManageHeight();
-                    Game.Window.Title = Position.ToString();
-                }
+                MouseFunctions();
+                KeyboardFunctions();
+                
+                ManageHeight();
+                CreateLookAt();
+
+                Game.Window.Title = Position.ToString();
+
                 TimeElapsedSinceUpdate = 0;
             }
             base.Update(gameTime);
         }
 
-        private void ManageHeight()
-        {
-            Position = Grass.GetPositionWithHeight(Position, CHARACTER_HEIGHT);
-        }
-
+        //Mouse
         #region
-        private int ManageKey(Keys touche)
+        private void MouseFunctions()
         {
-            return InputMgr.IsPressed(touche) ? 1 : 0;
+            PreviousMousePosition = CurrentMousePosition;
+            CurrentMousePosition = InputMgr.GetPositionMouse();
+            DisplacementMouse = new Vector2(CurrentMousePosition.X - PreviousMousePosition.X,
+                                            CurrentMousePosition.Y - PreviousMousePosition.Y);
+
+            ManageMouseRotation();
+
+            CurrentMousePosition = new Point(Game.Window.ClientBounds.Width / 2, Game.Window.ClientBounds.Height / 2);
+            Mouse.SetPosition(CurrentMousePosition.X, CurrentMousePosition.Y);
+
         }
 
-        private void ManageAcceleration()
+        private void ManageMouseRotation()
         {
-            int accelerationValue = (ManageKey(Keys.Subtract) + ManageKey(Keys.OemMinus)) - (ManageKey(Keys.Add) + ManageKey(Keys.OemPlus));
-            if (accelerationValue != 0)
-            {
-                UpdateInterval += ACCELERATION * accelerationValue;
-                UpdateInterval = MathHelper.Max(STANDARD_UPDATE_INTERVAL, UpdateInterval);
-            }
+            ManageMouseYaw();
+            ManageMouseRoll();
         }
+
+        private void ManageMouseYaw()
+        {
+            Matrix yawMatrix = Matrix.Identity;
+
+            yawMatrix = Matrix.CreateFromAxisAngle(VerticalOrientation, DELTA_YAW * INITIAL_ROTATION_SPEED_SOURIS * -DisplacementMouse.X);
+
+            Direction = Vector3.Transform(Direction, yawMatrix);
+        }
+
+        private void ManageMouseRoll()
+        {
+            Matrix rollMatrix = Matrix.Identity;
+
+            rollMatrix = Matrix.CreateFromAxisAngle(Lateral, DELTA_ROLL * INITIAL_ROTATION_SPEED_SOURIS * -DisplacementMouse.Y);
+
+            Direction = Vector3.Transform(Direction, rollMatrix);
+        }
+        #endregion
+
+        //Keyboard
+        #region
+        private void KeyboardFunctions()
+        {
+            ManageDisplacement();
+            ManageKeyboardRotation();
+        } 
 
         private void ManageDisplacement()
         {
@@ -141,14 +153,13 @@ namespace HyperV
             Position -= lateralDisplacement * Lateral;
         }
 
-        private void ManageRotation()
+        private void ManageKeyboardRotation()
         {
-            ManageYaw();
-            ManagePitch();
-            ManageRoll();
+            ManageKeyboardYaw();
+            ManageKeyboardRoll();
         }
 
-        private void ManageYaw()
+        private void ManageKeyboardYaw()
         {
             Matrix yawMatrix = Matrix.Identity;
 
@@ -164,7 +175,7 @@ namespace HyperV
             Direction = Vector3.Transform(Direction, yawMatrix);
         }
 
-        private void ManagePitch()
+        private void ManageKeyboardRoll()
         {
             Matrix rollMatrix = Matrix.Identity;
 
@@ -178,32 +189,17 @@ namespace HyperV
             }
 
             Direction = Vector3.Transform(Direction, rollMatrix);
-            //VerticalOrientation = Vector3.Transform(VerticalOrientation, rollMatrix);
-        }
-
-        private void ManageRoll()
-        {
-            Matrix matriceRoulis = Matrix.Identity;
-
-            if (InputMgr.IsPressed(Keys.PageUp))
-            {
-                matriceRoulis = Matrix.CreateFromAxisAngle(Direction, DELTA_PITCH* INITIAL_ROTATION_SPEED);
-            }
-            if(InputMgr.IsPressed(Keys.PageDown))
-            {
-                matriceRoulis = Matrix.CreateFromAxisAngle(Direction, -DELTA_PITCH* INITIAL_ROTATION_SPEED);
-            }
-
-            VerticalOrientation = Vector3.Transform(VerticalOrientation, matriceRoulis);
-        }
-
-        private void KeyboardManagement()
-        {
-            if (InputMgr.IsNewKey(Keys.Z))
-            {
-                InZoom = !InZoom;
-            }
         }
         #endregion
+
+        private void ManageHeight()
+        {
+            Position = Grass.GetPositionWithHeight(Position, CHARACTER_HEIGHT);
+        }
+        private int ManageKey(Keys touche)
+        {
+            return InputMgr.IsPressed(touche) ? 1 : 0;
+        }
+
     }
 }

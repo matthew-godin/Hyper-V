4debc6e84d30d5ade2404fec7b67c12d39b9b75f
Anthony <Anthony@BUR-5>
2017-02-15 10:40:09 -0500

    l

--- HyperV/HyperV/GameProject.cs
+++ HyperV/HyperV/GameProject.cs
@@ -4,6 +4,7 @@ using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
+using GameProjectXNA;
 
 namespace HyperV
 {
--- HyperV/HyperV/Niveau.cs
+++ HyperV/HyperV/Niveau.cs
@@ -9,6 +9,7 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
 using System.IO;
+using AtelierXNA;
 
 
 namespace HyperV

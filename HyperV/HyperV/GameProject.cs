5f1c94f8ed185e5242d32691fa2c0d64aaf92af2
Matthew Godin <150533628+matthew-godin@users.noreply.github.com>
2017-04-22 15:17:23 -0400

    nothing

--- HyperV/HyperV/GameProject.cs
+++ HyperV/HyperV/GameProject.cs
@@ -165,7 +165,7 @@ namespace HyperV
             int startInd = parse.IndexOf("X:") + 2;
             float aXPosition = float.Parse(parse.Substring(startInd, parse.IndexOf(" Y") - startInd));
             startInd = parse.IndexOf("Y:") + 2;
-            float aYPosition = float.Parse(parse.Substring(startInd, parse.IndexOf("}") - startInd));
+            float aYPosition = float.Parse(parse.Substring(startInd, parse.IndexOf("}") - startInd));//nothing for push
             return new Vector2(aXPosition, aYPosition);
         }
 
